ba871f4cd37cbba2b821789e98e1f186f6caaf04
Zac Delagrange <zdelagrange@tenable.com>
2023-07-21 10:36:24 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3()
+
+const getObjectParams = {
+    Bucket: process.env.BUCKET,
+    Key: process.env.KEY
+};
+
+console.log(s3.config.endpoint)
+
+console.log('calling s3.getObject with params: ' + JSON.stringify(getObjectParams))
+
+s3.getObject(getObjectParams, (err, data) => {
+    if (err) {
+        throw(err)
+    }
+    console.log(data)
+})
